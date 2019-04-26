b502f0e03400f0820f3e0b0e1f7753c1ddb8a878
alexgimson86 <alexgimson@gmail.com>
2019-04-26 15:02:12 -0500

    added auto scrolling

--- personal_profile/src/components/chatInput.js
+++ personal_profile/src/components/chatInput.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Form, Button, Container, ListGroup, ButtonToolbar } from 'react-bootstrap'
+import { Form, Button, Row, Col,  Container, Card, ButtonToolbar } from 'react-bootstrap'
 import axios from 'axios'
 import io from 'socket.io-client';
 import uuid from 'uuid/v1';
+import '../styles/chatroom.css';
 import {
     BrowserRouter as Router,
     Redirect,
@@ -28,17 +29,19 @@ class Chat extends Component {
             .catch(err => {
                 console.log(err)
             }).then(results => {
-                let returnMessages = results.data.map((doc, i) => `${doc.username} said  ${doc.message}`)
+                let returnMessages = results.data.map((doc, i) => (<Row><Col>{doc.username} said</Col><Col>{doc.message}</Col></Row>))
                 this.setState({
                     messages: returnMessages
                 })
             })
+            this.scrollToBottom()
 
     }
+   
     initChat = () => {
         this.socket = io.connect('http://localhost:4000');
         this.socket.on('append message', (data) => {
-            let tempMessages = this.state.messages.concat( `${data.message.username} said ${data.message.message}`)
+            let tempMessages = this.state.messages.concat(<Row><Col>{data.message.username} said </Col><Col>{data.message.message}</Col></Row>)
             this.setState({
                 messages: tempMessages,
             })
@@ -52,8 +55,8 @@ class Chat extends Component {
             [event.target.name]: event.target.value
         })
     }
-
-
+    
+    
     sendMessage = (event) => {
         event.preventDefault();
         let user = sessionStorage.getItem('myCurrentUsername')
@@ -65,16 +68,20 @@ class Chat extends Component {
                 username: user
             }
         })
-
+        
         this.setState({
             message: ''
         })
-
-
+        
         this.socket.emit('chat message', {
             username: sessionStorage.getItem('myCurrentUsername'),
             message: this.state.message,
         })
+    }
+    componentDidUpdate(){
+        if(document.getElementById('refreshView')){
+            this.scrollToBottom();
+        }
     }
     goBack = () => {
         var un = sessionStorage.getItem('myCurrentUsername')
@@ -83,31 +90,45 @@ class Chat extends Component {
             backToList: link
         })
     }
+    scrollToBottom = () => {
+        document.getElementById('refreshView').scrollIntoView({ behavior: "smooth" });
+      }
+      
+      
+      
+     
     render() {
         if (this.state.backToList)
             return <Redirect to={this.state.backToList} />
         else
             return (
-                <Container>
-                    <Form>
-                        <Form.Group controlId="exampleForm.ControlTextarea1">
-                            <Form.Label>messenger input</Form.Label>
-                            <Form.Control onChange={this.handleChange} name="message" value={this.state.message} as="textarea" rows="3" />
-                        </Form.Group>
-                        <ButtonToolbar>
+                <div className="chatroom">
 
-                            <Button onClick={this.goBack} variant="secondary">Back</Button>
-                            <Button variant="primary" type="submit" onClick={this.sendMessage}>
-                                Submit
+                    <Container >
+                        <div id="scrollDown">
+                            {this.state.messages ? this.state.messages.map(messages => {
+                                return <Card body style={{margin:'10px' }} key={uuid()}>{messages}</Card>
+                            }) : ''}
+                        </div>
+                        <Container>
+                            <div>
+                                <br />
+                            <div id="refreshView"></div>
+                                <Form>
+                                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                                        <Form.Control onChange={this.handleChange} name="message" value={this.state.message} as="textarea" rows="3" />
+                                    </Form.Group>
+                                    <ButtonToolbar>
+                                        <Button onClick={this.goBack} variant="secondary">Back</Button>
+                                        <Button variant="primary" type="submit" onClick={this.sendMessage}>
+                                            Submit
   </Button>
-                        </ButtonToolbar>
-                    </Form>
-                    <ListGroup>
-                        {this.state.messages ? this.state.messages.map (messages => {
-                           return <ListGroup.Item key={uuid()}>{messages}</ListGroup.Item>
-                        }) : ''}
-                    </ListGroup>
-                </Container>
+                                    </ButtonToolbar>
+                                </Form>
+                            </div>
+                        </Container>
+                    </Container>
+                </div>
             );
     }
 }
