55ea162274ad587e009830f9a60a8da27bf3ff83
alexgimson86 <alexgimson@gmail.com>
2019-05-13 13:17:18 -0500

    addjobs component

--- personal_profile/src/components/addJobs.js
+++ personal_profile/src/components/addJobs.js
@@ -26,10 +26,23 @@ class RecruiterSignup extends Component {
             values[i].description = event.target.value
         else if(event.target.name === 'skills')
             values[i].skills = event.target.value
-            else if(event.target.name === 'date')
+        else if(event.target.name === 'date')
             values[i].date = event.target.value
+        else if(event.target.name === 'salary')
+            values[i].salary = event.target.value
         this.setState({ values });
         
+    }
+    deleteJob = (event) =>
+    {
+        var jobIndex = event.target.name
+
+        var newJobList = this.state.jobs
+        newJobList.splice(jobIndex,1)
+        this.setState({
+            jobs: newJobList
+        })
+
     }
    appendJobForm = () => {
     var arr = [...this.state.jobs]
@@ -37,7 +50,8 @@ class RecruiterSignup extends Component {
         title: '',
         skills:'',
         description: '',
-        date: ''
+        date: '',
+        salary: '',
     }
     arr.push(job)
     this.setState({
@@ -50,12 +64,18 @@ class RecruiterSignup extends Component {
         var descriptionId = 'description#'
         var skillsId ='skills#'
         var dateId = 'date#'
+        var salaryId = 'salary#'
         return this.state.jobs.map((job , i )=>
-        <li key={`job#${i}`} >
+        <li  key={`job#${i}`} >
           <div><input type="text" id={`${titleId}${i}`} onChange={this.handleChange} value={job.title || ''} className="form-control"   name="title"  placeholder="Enter Job Title" />
             <input type="textarea"   id={`${descriptionId}${i}`} name="description" onChange = {this.handleChange} value = {job.description} className="form-control"  aria-describedby="job description" placeholder="Enter Job description" />
             <input type="textarea" id={`${skillsId}${i}`} name="skills" onChange ={this.handleChange} value={job.skills} className="form-control"  aria-describedby="required skills" placeholder="Enter required skills" />
             <input type="date" id={`${dateId}${i}`} name="date" onChange ={this.handleChange}className="form-control"  aria-describedby="date" placeholder={date} />
+            <input type="text" id={`${salaryId}${i}`} name="salary" onChange ={this.handleChange}className="form-control"  aria-describedby="salary" placeholder="Enter Salary" />
+            <Button variant="warning" type="button" name={i} onClick={this.deleteJob}>delete</Button>
+            <hr/>
+            
+
         <br/></div>
         </li>
         )
@@ -103,11 +123,15 @@ class RecruiterSignup extends Component {
                  <form onSubmit={this.handleSubmit}>
 
                     <Button style={bStyle} onClick={this.appendJobForm}>Add Job</Button>
+                    <div>
                         <ol>
-
                         {this.addJob()}
                         </ol>
-                    <input type="submit" value="Submit" />
+                    </div>
+
+                    <Button variant="secondary" type="submit">
+                        Submit
+                    </Button>
                  </form>
                 </div>
 
@@ -117,7 +141,7 @@ class RecruiterSignup extends Component {
                 <Resume username={this.props.match.params.username} />
                 <br />
                 <hr />
-                <button onClick={this.returnToList}>Done</button>
+                <Button variant="success" onClick={this.returnToList}>Done</Button>
             </div>
         );
     }
